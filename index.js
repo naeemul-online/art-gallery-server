5fa03f7ca8041d9bdf20b93e21b4935416b4ced8
naeemul-online <naeemul.online@gmail.com>
2024-04-28 18:04:07 +0300

    delete operation added

--- index.js
+++ index.js
@@ -82,6 +82,19 @@ async function run() {
       res.send(result);
     });
 
+
+    // delete operation
+    app.delete("/delete/:id", async(req, res) => {
+        const result = await craftCollection.deleteOne({
+            _id: new ObjectId(req.params.id)
+        })
+        console.log(result);
+        res.send(result)
+
+    })
+
+
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
